b1694afdded67576d3d9bb68cb71506478477dc1
Himanshu Garg <himanshugarg02@gmail.com>
2020-10-20 22:35:44 +0530

    Updates

---
+++ precache-manifest.c4acb76cb755b37ae15862afa1aeffc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd039c8ac7745ca33d2dbf8abb2de18b",
+    "url": "/checkbox/index.html"
+  },
+  {
+    "revision": "bde07457f304724e6e5d",
+    "url": "/checkbox/static/css/main.abb36ab6.chunk.css"
+  },
+  {
+    "revision": "9fd595d361f09fd2f7df",
+    "url": "/checkbox/static/js/2.eedc338c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/checkbox/static/js/2.eedc338c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bde07457f304724e6e5d",
+    "url": "/checkbox/static/js/main.e80b6125.chunk.js"
+  },
+  {
+    "revision": "f043544c772928d3707d",
+    "url": "/checkbox/static/js/runtime-main.b6016b7e.js"
+  }
+]);
